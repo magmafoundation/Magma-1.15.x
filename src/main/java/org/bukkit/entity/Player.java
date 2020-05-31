5aab8ddbbf516181fd4398f3eb192fd14e5d1393
Hexeption <minecraftfun201@gmail.com>
2020-05-31 15:50:52 +0100

    Add Respawn API

--- src/main/java/org/bukkit/entity/Player.java
+++ src/main/java/org/bukkit/entity/Player.java
@@ -1284,6 +1284,13 @@ public interface Player extends HumanEntity, Conversable, OfflinePlayer, PluginM
             throw new UnsupportedOperationException("Not supported yet.");
         }
 
+        /**
+         * Respawns the player if dead.
+         */
+        public void respawn() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
     }
     @NotNull
     @Override
