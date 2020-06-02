9a74fba46cfe8bb398074e59f8ea3378f3d00bc1
Hexeption <minecraftfun201@gmail.com>
2020-06-02 18:57:51 +0100

    0019-Allow-Disabling-of-Command-TabComplete

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -1641,6 +1641,13 @@ public final class CraftServer implements Server {
     }
 
     public List<String> tabCompleteCommand(Player player, String message, ServerWorld world, Vec3d pos) {
+        // Spigot Start
+        if ( (org.spigotmc.SpigotConfig.tabComplete < 0 || message.length() <= org.spigotmc.SpigotConfig.tabComplete) && !message.contains( " " ) )
+        {
+            return ImmutableList.of();
+        }
+        // Spigot End
+
         List<String> completions = null;
         try {
             if (message.startsWith("/")) {
